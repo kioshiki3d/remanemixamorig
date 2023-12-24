e9c2f4eacf8fd803817faf31feb679d943773db2
yoshikik123 <yoshiki.k123@gmail.com>
2023-12-24 17:42:35 +0900

    add readme, dissolve chest

---
+++ README.md
@@ -0,0 +1,19 @@
+# 使い方
+
+3D viewからKJaddonを選択
+armatureをactiveにする
+remaneを実行する
+
+# 機能
+"mixamorig"の文字が消える
+"Right"、"Left"表記が"R"、"L"表記に代わる
+RLになり対称操作ができるようになる
+
+# 履歴
+v0.1 2021/3/28
+first  release
+
+v0.2 2023/12/24
+詳細コード整理
+Upper、Lower ChestをChestに変換
+
--- mixamorig/rename_mixamorig.py
+++ mixamorig/rename_mixamorig.py
@@ -31,15 +31,18 @@ class KJ_Rename_PT_Panel(Panel):
 
     def draw(self, context):
         layout = self.layout
+        # Eyedropper要検討
+        #scene = context.scene
+        #layout.prop(scene, "target", text="", icon='ARMATURE_DATA') 
+        #layout.separator()
         layout.label(text="rename mixamorig")
         layout.operator(KJ_rename_bone.bl_idname, text="rename")
+        layout.separator()
+        layout.label(text="dissolve upperchest")
+        layout.operator(KJ_dissolve_chest.bl_idname, text="dissolve")
 
 
-class KJ_rename_bone(Operator):
-    bl_idname = "kjrename.operator"
-    bl_label = "KJ_rename_bone"
-
-
+class KJ_rename_bone_base(Operator):
     def execute(self, context):
         obj = bpy.context.active_object
         for bone in obj.data.bones:
@@ -71,7 +74,37 @@ class KJ_rename_bone(Operator):
         return {"FINISHED"}
 
 
-classes = [KJ_Rename_PT_Panel,KJ_rename_bone]
+class KJ_rename_bone(KJ_rename_bone_base):
+    bl_idname = "kjrename.operator"
+    bl_label = "KJ_rename_bone"
+
+
+    def execute(self, context):
+        super().execute(context)
+        return {"FINISHED"}
+
+
+class KJ_dissolve_chest(KJ_rename_bone_base):
+    bl_idname = "kjdissolve.operator"
+    bl_label = "KJ_dissolve_chest"
+
+
+    def execute(self, context):
+        super().execute(context)
+        obj = bpy.context.active_object
+        for bone in obj.data.bones:
+            bonename = bone.name
+
+            if "UpperChest" in bonename:
+                bpy.ops.armature.dissolve()
+            if "LowerChest" in bonename:
+                bonename = bonename.replace("Lower", "")
+                bone.name = bonename + "_R" 
+            
+        return {"FINISHED"}
+
+
+classes = [KJ_Rename_PT_Panel, KJ_rename_bone, KJ_dissolve_chest]
 
 
 def register():
